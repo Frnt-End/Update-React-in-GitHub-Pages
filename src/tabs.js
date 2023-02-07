694ccaa7f426cca256f20ab928e2997a96e05370
Nirit Nagar <nirit.nn@gmail.com>
2023-02-07 23:01:29 +0100

    upadte path links

--- src/tabs.js
+++ src/tabs.js
@@ -1,54 +1,50 @@
-import React from 'react';
-import AuthorImg  from './author.png';
-
-
+import React from "react";
+import AuthorImg from "./author.png";
 
 const Portfolio = () => {
-  window.open(
-              "https://nirit.website", "_blank");
-}
-
+  window.open("https://frnt-end.github.io/portfolio/", "_blank");
+};
 
 export function Tutorial() {
   return (
     <div>
       <pre>
-  <code>
-  {`npm run deploy`}
-  </code>
-</pre>
+        <code>{`npm run deploy`}</code>
+      </pre>
 
-<pre>
-<code>
-{`git add .`}
-</code>
-</pre>
+      <pre>
+        <code>{`git add .`}</code>
+      </pre>
 
-<pre>
-<code>
-{`git commit -m "update build for gh-pages"`}
-</code>
-</pre>
+      <pre>
+        <code>{`git commit -m "update build for gh-pages"`}</code>
+      </pre>
 
-<pre>
-<code>
-{`git push -u origin master`}
-</code>
-</pre>
+      <pre>
+        <code>{`git push -u origin master`}</code>
+      </pre>
     </div>
-  )
+  );
 }
 
 export function Author() {
   return (
     <div className="author-container">
       <div>
-      <img src={AuthorImg} alt="Autor" />
-      <p>Creative & Code:)
-      <br /><a href="https://nirit.website" target="_blank">nirit.website</a>
-      </p>
+        <img src={AuthorImg} alt="Autor" />
+        <p>
+          Creative & Code:)
+          <br />
+          <a
+            href="https://frnt-end.github.io/portfolio/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            nirit.website
+          </a>
+        </p>
       </div>
       <button onClick={Portfolio}>Portfolio</button>
     </div>
-  )
+  );
 }
